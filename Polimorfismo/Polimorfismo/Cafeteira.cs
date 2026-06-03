c2b0465d4c3db4a39c952bccdf7bb943f27e0617
LeoMoino <126733969+LeoMoino@users.noreply.github.com>
2026-06-03 15:45:56 -0300

    Estudos Polimorfismo

---
+++ Polimorfismo/Polimorfismo/Cafeteira.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class Cafeteira : Eletrodomestico
+    {
+        public Cafeteira(string nome) : base(nome)
+        {
+        }
+        private static void AquecerAgua()
+        {
+            Console.WriteLine("Aquecendo a água...");
+        }
+
+        private static void ColocarCapsula()
+        {
+            Console.WriteLine("Colocando a cápsula de café...");
+        }
+
+        public void PrepararCafe()
+        {
+            AquecerAgua();
+            ColocarCapsula();
+            Console.WriteLine("Café preparado!");
+        }
+
+        //Override significa que eu posso sobrescrever ele, ou seja, eu posso implementar ele de uma forma diferente do que foi definido na classe abstrata
+        public override void Ligar()
+        {
+            //regra de negócio para ligar a cafeteira
+            throw new NotImplementedException();
+        }
+
+        public override void Desligar()
+        {
+            //regra de negócio para desligar a cafeteira
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Polimorfismo/Polimorfismo/Eletrodomestico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public abstract class Eletrodomestico
+    {
+
+        private readonly string _nome;
+
+        public Eletrodomestico(string nome)
+        {
+            _nome = nome;
+        }
+
+
+        //abstract pq não tem implementação, cada classe filha tem que implementar
+        public abstract void Ligar();   
+        public abstract void Desligar();
+
+    }
+}
